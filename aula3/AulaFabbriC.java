0f41c53a64e397526190f18cd2615c1c47d3d861
bernardo-333 <bernardinho.freitas11@gmail.com>
2025-06-02 08:46:35 -0300

    Update and rename aulaFabbriC.java to AulaFabbriC.java

--- aula3/AulaFabbriC.java
+++ aula3/AulaFabbriC.java
@@ -2,7 +2,7 @@ package aula3;
 
 import java.util.Scanner;
 
-public class aulaFabbriC {
+public class AulaFabbriC {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
